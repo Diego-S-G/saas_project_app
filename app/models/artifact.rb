e228f1ca68daf245f0efab45117cb3e08412ec4d
Diego-S-G <diego07goncalves@gmail.com>
2024-12-04 14:23:50 -0300

    Coloca alguns códigos comentados das coisas que foram colocadas no projeto do professor para ir pro AWS S3 e afins, mas como não estou fazendo isso e nem usando Heroku só contornei, focando mais no multi-tenancy.

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,4 +1,5 @@
 class Artifact < ApplicationRecord
+  # before_save :upload_to_s3
   attr_accessor :upload
   belongs_to :project
 
@@ -15,4 +16,16 @@ class Artifact < ApplicationRecord
       errors.add(:upload, "File size must no more than 10MB") unless upload.size <= self.class::MAX_FILESIZE
     end
   end
+
+  # def upload_to_s3 # pro aws s3 bucket e afins
+  #   s3 = Aws::S3::Resource.new
+    
+  #   tenant_name = Tenant.find(Thread.current[:tenant_id]).name
+    
+  #   obj = s3.bucket(ENV['AWS_S3_BUCKET']).object("#{tenant_name}/#{upload.original_filename}")
+    
+  #   obj.upload_file(upload.path, acl:'public-read')
+    
+  #   self.key = obj.public_url
+  # end
 end
