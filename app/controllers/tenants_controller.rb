54eb22a4a2bc3c820b1e767b3b9536e35808d21b
Diego-S-G <diego07goncalves@gmail.com>
2024-12-04 15:16:22 -0300

    Faz o update para poder atualizar nome da organização/plano.

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -4,6 +4,23 @@ class TenantsController < ApplicationController
   def edit
   end
 
+  def update
+    respond_to do |format|
+      if @tenant.update(tenant_params)
+        format.html { redirect_to root_path, notice: "Tenant was successfully updated." }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+  
+  # def change
+  #   @tenant = Tenant.find(params[:id])
+  #   Tenant.set_current_tenant @tenant.id
+  #   session[:tenant_id] = Tenant.current_tenant.id
+  #   redirect_to home_index_path, notice: "Switched to organization #{@tenant.name}"
+  # end
+
   private
 
   def set_tenant
