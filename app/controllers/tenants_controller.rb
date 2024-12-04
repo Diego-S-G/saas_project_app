69d5cef0548775fe2cce69840e53559bc79e608a
Diego-S-G <diego07goncalves@gmail.com>
2024-12-04 14:58:08 -0300

    Adiciona tenants_controller, começa a trabalhar na questão de atualizar plano e faz a edit page de tenants, sem as coisas do stripe pois não vou usar que nem o professor.

---
+++ app/controllers/tenants_controller.rb
@@ -0,0 +1,16 @@
+class TenantsController < ApplicationController
+  before_action :set_tenant
+
+  def edit
+  end
+
+  private
+
+  def set_tenant
+    @tenant = Tenant.find(Tenant.current_tenant_id)
+  end
+  
+  def tenant_params
+    params.require(:tenant).permit(:name, :plan)
+  end
+end
