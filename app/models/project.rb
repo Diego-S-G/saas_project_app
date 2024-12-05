c7121fc3cc2c056eb24cf3144a9103a3e9a5fa0f
Diego-S-G <diego07goncalves@gmail.com>
2024-12-05 09:31:27 -0300

    Adiciona scaffold de UserProject e faz associação m2m entre user e project, mexe em rotas e no método by_user_plan_and_tenant e mexe nos controllers envolvidos.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ApplicationRecord
   belongs_to :tenant
   has_many :artifacts, dependent: :destroy
+  has_many :user_projects
+  has_many :users, through: :user_projects
 
   validates_uniqueness_of :title
   validate :free_plan_can_only_have_one_project
@@ -11,12 +13,21 @@ class Project < ApplicationRecord
     end
   end
 
-  def self.by_plan_and_tenant(tenant_id) # placeholder por enquanto
+  def self.by_user_plan_and_tenant(tenant_id, user)
     tenant = Tenant.find(tenant_id)
     if tenant.plan == 'premium'
-      tenant.projects
+      if user.is_admin?
+        tenant.projects
+      else
+        user.projects.where(tenant_id: tenant.id)
+      end
     else
-      tenant.projects.order(:id).limit(1)
+      project = tenant.projects.order(:id).limit(1) # aqui peguei a parte final de um estudante pois ele disse que teria um problema lá na frente. Aula 371
+      if user.is_admin?
+        project
+      else
+        (user.projects.where(tenant_id: tenant_id).include?(project[0])) ? project : []
+      end
     end
   end
 end
